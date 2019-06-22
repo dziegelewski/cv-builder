0458e49de715a9c15e7418bc5725237c50067044
Grzegorz Dziegelewski <gdziegelewski@gmail.com>
2019-06-22 10:09:26 +0200

    Adjustments

--- src/utils/index.js
+++ src/utils/index.js
@@ -33,20 +33,9 @@ export const toOnOff = (isOn) => {
 export const mapTags = (tags) => {
   const { CONFIDENTIAL, ...onlyTags } = tags;
   const abbrevs = Object.keys(onlyTags);
-  
+
   return abbrevs.map((abbrev) => ({
     name: tags[abbrev],
     value: tags[abbrev]
   }))
 }
-
-export const showHelp = () => alert`
-  To see your data here:
-  1. Get the repo
-  2. Edit source code in "src/cv-data"
-  3. You might also want to e.g. edit fonts or colors in "src/components/Styles/variables"
-  4. Run "npm start"
-  5. And just observe the results in the browser!
-  6. Display legal clause/photo or highlight technologies of your choice depending of your needs
-  7. Finally "Download" the result.
-`;
